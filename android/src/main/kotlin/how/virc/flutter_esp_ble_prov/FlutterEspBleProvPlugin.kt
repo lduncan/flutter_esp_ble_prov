d9b74af7503efb78e9d5487cab866bd22be4e29e
Ali Afshar <aafshar@gmail.com>
2023-01-19 11:22:08 +0000

    Remove unused implementation and fixed some logging calls.

--- android/src/main/kotlin/how/virc/flutter_esp_ble_prov/FlutterEspBleProvPlugin.kt
+++ android/src/main/kotlin/how/virc/flutter_esp_ble_prov/FlutterEspBleProvPlugin.kt
@@ -1,20 +1,14 @@
 package how.virc.flutter_esp_ble_prov
 
-
-import android.Manifest
 import android.app.Activity
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.le.ScanResult
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.annotation.NonNull
-import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.espressif.provisioning.DeviceConnectionEvent
 import com.espressif.provisioning.ESPConstants
@@ -27,7 +21,6 @@ import com.espressif.provisioning.listeners.WiFiScanListener
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
-import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -66,50 +59,41 @@ abstract class ActionManager(val boss: Boss) {
 }
 
 
-class Boss(val context: Context, val activity : Activity) {
+class Boss {
 
   private val logTag = "FlutterEspBleProv"
-
   private val scanBleMethod = "scanBleDevices"
   private val scanWifiMethod = "scanWifiNetworks"
   private val provisionWifiMethod = "provisionWifi"
+  private val platformVersionMethod = "getPlatformVersion"
 
   val devices = mutableMapOf<String, BleConnector>();
   val networks = mutableSetOf<String>();
 
-  val bleScanner: BleScanManager
-  val wifiScanner: WifiScanManager
-  val wifiProvisioner : WifiProvisionManager
+  private val bleScanner: BleScanManager = BleScanManager(this)
+  private val wifiScanner: WifiScanManager = WifiScanManager(this)
+  private val wifiProvisioner: WifiProvisionManager = WifiProvisionManager(this)
 
-  lateinit var lastConn: BleConnector
+  private lateinit var platformContext: Context
+  private lateinit var platformActivity: Activity
 
-  init {
-    bleScanner = BleScanManager(this)
-    wifiScanner = WifiScanManager(this)
-    wifiProvisioner = WifiProvisionManager(this)
-  }
-
-  val provisionManager get() = ESPProvisionManager.getInstance(context)
-  val esp get() = provisionManager.espDevice
+  val espManager: ESPProvisionManager get() = ESPProvisionManager.getInstance(platformContext)
 
   fun d(msg: String) = Log.d(logTag, msg)
   fun e(msg: String) = Log.e(logTag, msg)
   fun i(msg: String) = Log.e(logTag, msg)
 
-  fun create(): ESPDevice {
-    return ESPProvisionManager.getInstance(context).createESPDevice(
-      ESPConstants.TransportType.TRANSPORT_BLE,
-      ESPConstants.SecurityType.SECURITY_1
-    )
-  }
 
   fun connector(deviceName: String): BleConnector? {
     return devices[deviceName]
   }
 
-  fun connect(conn: BleConnector, pop : String, callback: (ESPDevice) -> Unit) {
-    lastConn = conn
-    val esp = create()
+  fun connect(
+    conn: BleConnector, proofOfPossession: String, onConnectCallback: (ESPDevice) -> Unit
+  ) {
+    val esp = espManager.createESPDevice(
+      ESPConstants.TransportType.TRANSPORT_BLE, ESPConstants.SecurityType.SECURITY_1
+    )
     EventBus.getDefault().register(object {
       @Subscribe(threadMode = ThreadMode.MAIN)
       fun onEvent(event: DeviceConnectionEvent) {
@@ -117,8 +101,8 @@ class Boss(val context: Context, val activity : Activity) {
         when (event.eventType) {
           ESPConstants.EVENT_DEVICE_CONNECTED -> {
             EventBus.getDefault().unregister(this)
-            esp.proofOfPossession = pop
-            callback(esp)
+            esp.proofOfPossession = proofOfPossession
+            onConnectCallback(esp)
           }
         }
       }
@@ -129,31 +113,44 @@ class Boss(val context: Context, val activity : Activity) {
   fun call(call: MethodCall, result: Result) {
     val ctx = CallContext(call, result)
     when (call.method) {
+      platformVersionMethod -> getPlatformVersion(ctx)
       scanBleMethod -> bleScanner.call(ctx)
       scanWifiMethod -> wifiScanner.call(ctx)
       provisionWifiMethod -> wifiProvisioner.call(ctx)
+      else -> result.notImplemented()
     }
   }
 
+  private fun getPlatformVersion(ctx: CallContext) {
+    ctx.result.success("Android ${android.os.Build.VERSION.RELEASE}")
+  }
+
+  fun attachActivity(activity: Activity) {
+    platformActivity = activity
+  }
+
+  fun attachContext(context: Context) {
+    platformContext = context
+  }
 }
 
 
 class BleScanManager(boss: Boss) : ActionManager(boss) {
 
   override fun call(ctx: CallContext) {
-    ActivityCompat.requestPermissions(
-      boss.activity,
-      arrayOf(
-        Manifest.permission.BLUETOOTH_SCAN,
-        Manifest.permission.BLUETOOTH_CONNECT,
-        Manifest.permission.BLUETOOTH
-      ),
-      12345
-    )
+    //ActivityCompat.requestPermissions(
+    //  boss.platformActivity,
+    //  arrayOf(
+    //    Manifest.permission.BLUETOOTH_SCAN,
+    //    Manifest.permission.BLUETOOTH_CONNECT,
+    //    Manifest.permission.BLUETOOTH
+    //  ),
+    //  12345
+    //)
     boss.d("searchBleEspDevices: start")
     val prefix = ctx.arg("prefix") ?: return
 
-    boss.provisionManager.searchBleEspDevices(prefix, object : BleScanListener{
+    boss.espManager.searchBleEspDevices(prefix, object : BleScanListener {
       override fun scanStartFailed() {
         TODO("Not yet implemented")
       }
@@ -186,25 +183,24 @@ class WifiScanManager(boss: Boss) : ActionManager(boss) {
     boss.d("esp connect: start")
     boss.connect(conn, proofOfPossession) { esp ->
       boss.d("scanNetworks: start")
-      esp.scanNetworks(
-        object : WiFiScanListener {
-          override fun onWifiListReceived(wifiList: ArrayList<WiFiAccessPoint>?) {
-            wifiList ?: return
-            wifiList.forEach { boss.networks.add(it.wifiName) }
-            boss.d("scanNetworks: complete ${boss.networks}")
-            Handler(Looper.getMainLooper()).post {
+      esp.scanNetworks(object : WiFiScanListener {
+        override fun onWifiListReceived(wifiList: ArrayList<WiFiAccessPoint>?) {
+          wifiList ?: return
+          wifiList.forEach { boss.networks.add(it.wifiName) }
+          boss.d("scanNetworks: complete ${boss.networks}")
+          Handler(Looper.getMainLooper()).post {
             // Call the desired channel message here.
-              ctx.result.success(ArrayList<String>(boss.networks))
-            }
-            boss.d("scanNetworks: complete 2 ${boss.networks}")
-            esp.disconnectDevice()
+            ctx.result.success(ArrayList<String>(boss.networks))
           }
-          override fun onWiFiScanFailed(e: java.lang.Exception?) {
-            boss.e("scanNetworks: error $e")
-            ctx.result.error("E1", "WiFi scan failed", "Exception details $e")
-          }
+          boss.d("scanNetworks: complete 2 ${boss.networks}")
+          esp.disconnectDevice()
         }
-      )
+
+        override fun onWiFiScanFailed(e: java.lang.Exception?) {
+          boss.e("scanNetworks: error $e")
+          ctx.result.error("E1", "WiFi scan failed", "Exception details $e")
+        }
+      })
     }
   }
 }
@@ -221,7 +217,7 @@ class WifiProvisionManager(boss: Boss) : ActionManager(boss) {
 
     boss.connect(conn, proofOfPossession) { esp ->
       boss.d("provision: start")
-      esp.provision(ssid, passphrase, object : ProvisionListener{
+      esp.provision(ssid, passphrase, object : ProvisionListener {
         override fun createSessionFailed(e: java.lang.Exception?) {
           boss.e("wifiprovision createSessionFailed")
         }
@@ -261,129 +257,6 @@ class WifiProvisionManager(boss: Boss) : ActionManager(boss) {
 
 }
 
-class ProvisionManager(boss: Boss) : ActionManager(boss) {
-  override fun call(ctx: CallContext) {
-    TODO("Not yet implemented")
-  }
-
-}
-
-
-class ListensToBle(val result: Result) : BleScanListener {
-
-  private val TAG = "FlutterEspBleProv"
-
-  val devices: MutableMap<String, BleConnector>
-
-  init {
-    devices = hashMapOf()
-  }
-
-  override fun scanStartFailed() {
-    result.success("scan failed")
-  }
-
-  override fun onPeripheralFound(device: BluetoothDevice?, scanResult: ScanResult?) {
-    Log.e(TAG, "peripheral found $device $scanResult ${device?.name}")
-    if (device != null && scanResult != null && !devices.containsKey(device.name)) {
-      devices.put(device.name, BleConnector(device, scanResult))
-    }
-  }
-
-  override fun scanCompleted() {
-    Log.e(TAG, "scan complete")
-    //val res : ArrayList<String> = arrayListOf()
-    //for (k in devices.keys) {
-    //  res.add(k)
-    //}
-    val res: List<String> = ArrayList(devices.keys)
-    result.success(res)
-  }
-
-  override fun onFailure(e: Exception?) {
-    result.error("error", e?.message, e)
-  }
-
-}
-
-class ListensToWifi(fResult: Result) : WiFiScanListener {
-
-  private val TAG = "FlutterEspBleProv"
-
-  val networks: MutableList<WiFiAccessPoint>
-  val result: Result
-
-
-  init {
-    result = fResult
-    networks = arrayListOf()
-  }
-
-  override fun onWifiListReceived(wifiList: ArrayList<WiFiAccessPoint>?) {
-    if (wifiList != null) {
-      networks.addAll(wifiList)
-    }
-    Log.e(TAG, "$networks")
-    val ssids: ArrayList<String> = arrayListOf()
-    networks.forEach({ ssids.add(it.wifiName) })
-    result.success(ssids)
-  }
-
-  override fun onWiFiScanFailed(e: java.lang.Exception?) {
-    TODO("Not yet implemented")
-  }
-
-}
-
-class ListensToProvisioning : ProvisionListener {
-  private val TAG = "FlutterEspBleProv"
-
-  override fun createSessionFailed(e: java.lang.Exception?) {
-    Log.e(TAG, "session failed")
-
-  }
-
-  override fun wifiConfigSent() {
-    Log.e(TAG, "config sent")
-  }
-
-  override fun wifiConfigFailed(e: java.lang.Exception?) {
-    Log.e(TAG, "config failed")
-
-  }
-
-  override fun wifiConfigApplied() {
-    Log.e(TAG, "config applied")
-
-  }
-
-  override fun wifiConfigApplyFailed(e: java.lang.Exception?) {
-    Log.e(TAG, "config apply failed")
-
-  }
-
-  override fun provisioningFailedFromDevice(failureReason: ESPConstants.ProvisionFailureReason?) {
-    Log.e(TAG, "provision faiuled from device")
-
-  }
-
-  override fun deviceProvisioningSuccess() {
-    Log.e(TAG, "provision success")
-
-  }
-
-  override fun onProvisioningFailed(e: java.lang.Exception?) {
-    Log.e(TAG, "provision failed")
-
-  }
-
-}
-
-
-class DevicesManager {
-
-}
-
 
 /** FlutterEspBleProvPlugin */
 class FlutterEspBleProvPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
@@ -393,65 +266,32 @@ class FlutterEspBleProvPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
 
-  private val TAG = "FlutterEspBleProv"
-
-  private lateinit var boss: Boss
-
+  private val logTag = "FlutterEspBleProvChannel"
+  private val boss = Boss()
   private lateinit var channel: MethodChannel
-  private lateinit var context: Context
 
-  private lateinit var activity: Activity
-  private lateinit var devices: Map<String, BleConnector>
-  private lateinit var networks: List<WiFiAccessPoint>
-  private lateinit var esp: ESPDevice
-  private lateinit var wifiListener: ListensToWifi
-  private lateinit var eventChannel: EventChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_esp_ble_prov")
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    Log.d(logTag, "onAttachedToEngine: $binding")
+    channel = MethodChannel(binding.binaryMessenger, "flutter_esp_ble_prov")
     channel.setMethodCallHandler(this)
-
-
-
-    context = flutterPluginBinding.applicationContext
-    var pv = ESPProvisionManager.getInstance(context)
-    Log.e(TAG, "ASttach")
-
-
+    boss.attachContext(binding.applicationContext)
   }
 
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    when (call.method) {
-      "getPlatformVersion" -> getPlatformVersion(result)
-      "scanBleDevices" -> boss.call(call, result)
-      "scanWifiNetworks" -> boss.call(call, result)
-      "provisionWifi" -> boss.call(call, result)
-      else -> result.notImplemented()
-    }
-
-//    if (call.method == "getPlatformVersion") {
-//      //result.success("Android ${android.os.Build.VERSION.RELEASE}")
-//      Log.d("BLE", "ali")
-//      //result.success("$context")
-//      result.success(methods.getPlatformVersion())
-//    } else {
-//      result.notImplemented()
-//    }
-  }
-
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    Log.d(logTag, "onDetachedFromEngine: $binding")
     channel.setMethodCallHandler(null)
   }
 
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    Log.d(logTag, "onMethodCall: ${call.method} ${call.arguments}")
+    boss.call(call, result)
+  }
+
   override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    Log.e(TAG, "attach")
-    activity = binding.activity
+    Log.d(logTag, "onAttachedToActivity: $binding")
     binding.addActivityResultListener(this)
-    boss = Boss(context, binding.activity)
-
-
+    boss.attachActivity(binding.activity)
   }
 
   override fun onDetachedFromActivityForConfigChanges() {
@@ -467,102 +307,15 @@ class FlutterEspBleProvPlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
   }
 
   override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?): Boolean {
-    Log.e(TAG, "onActivityResult $requestCode $resultCode $data")
+    Log.d(logTag, "onActivityResult $requestCode $resultCode $data")
     return false
   }
 
   override fun onRequestPermissionsResult(
-    requestCode: Int,
-    permissions: Array<out String>,
-    grantResults: IntArray
+    requestCode: Int, permissions: Array<out String>, grantResults: IntArray
   ): Boolean {
-    Log.e(TAG, "on permission result")
+    Log.d(logTag, "onPermissionResult $requestCode $permissions $grantResults")
     return true
   }
 
-  fun getPlatformVersion(result: Result) {
-    return result.success("Android ${android.os.Build.VERSION.RELEASE}")
-  }
-
-  fun scanBle(result: Result) {
-    Log.d(TAG, "scanning BLE")
-    // Register the permissions callback, which handles the user's response to the
-// system permissions dialog. Save the return value, an instance of
-// ActivityResultLauncher. You can use either a val, as shown in this snippet,
-// or a lateinit var in your onAttach() or onCreate() method.
-
-
-    ActivityCompat.requestPermissions(
-      activity,
-      arrayOf(
-        Manifest.permission.BLUETOOTH_SCAN,
-        Manifest.permission.BLUETOOTH_CONNECT,
-        Manifest.permission.BLUETOOTH
-      ),
-      12345
-    )
-    val bleListener = ListensToBle(result)
-    devices = bleListener.devices
-    ESPProvisionManager.getInstance(context).searchBleEspDevices("PROV_", bleListener)
-    //return result.success("Android ${android.os.Build.VERSION.RELEASE}")
-  }
-
-  fun scanWifi(call: MethodCall, result: Result) {
-    val deviceName = call.argument<String>("deviceName")
-    if (deviceName == null) {
-      result.error("E0", "No deviceName given.", "no deviceName given")
-      return
-    }
-    val conn = devices[deviceName]
-    if (conn == null) {
-      result.error("E1", "Device not found.", "Device not found")
-      return
-    }
-
-    EventBus.getDefault().register(this)
-
-    esp = ESPProvisionManager.getInstance(context).createESPDevice(
-      ESPConstants.TransportType.TRANSPORT_BLE,
-      ESPConstants.SecurityType.SECURITY_1
-    )
-    esp.connectBLEDevice(conn.device, conn.primaryServiceUuid)
-
-
-    wifiListener = ListensToWifi(result)
-    networks = wifiListener.networks
-
-  }
-
-
-  private fun provisionWifi(call: MethodCall, result: Result) {
-    val ssid = call.argument<String>("ssid")
-    if (ssid == null) {
-      result.error("E0", "No ssid given.", "no ssid given")
-      return
-    }
-    val passphrase = call.argument<String>("passphrase")
-    if (passphrase == null) {
-      result.error("E0", "No passphrase given", "no passphrase given")
-      return
-    }
-    val listener = ListensToProvisioning()
-    ESPProvisionManager.getInstance(context).espDevice.provision(ssid, passphrase, listener)
-    //esp.provision(ssid, passphrase, listener)
-    result.success(true)
-
-  }
-
-
-  fun espConnected() {
-    Log.e(TAG, "ESP CONNECTED!")
-    esp.proofOfPossession = "abcd1234"
-    esp.scanNetworks(wifiListener)
-  }
-
-  @Subscribe(threadMode = ThreadMode.MAIN)
-  fun onEvent(event: DeviceConnectionEvent) {
-    when (event.eventType) {
-      ESPConstants.EVENT_DEVICE_CONNECTED -> espConnected();
-    }
-  }
 }
